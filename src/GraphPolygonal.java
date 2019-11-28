35fadbecf6a8c27cf717d4c794877d2e9a565ba4
YeongSan <apua34@gmail.com>
2019-11-29 03:52:33 +0900

    코드 분할 완료

--- src/GraphPolygonal.java
+++ src/GraphPolygonal.java
@@ -1,113 +1,16 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-<<<<<<< HEAD
-// 변수 데이터 여기에 선언
-class MainGraph_Stick extends JPanel {
-	int NO2 = 100;
-	int OZ = 70;
-	int CO2 = 32;
-	int SO2 = 22;
-	int Fd = 45;
-	int Ud = 100;
-
-// 대충 이걸로 조절하려고 했는데 작동안하쥬~~ 토요일 해야쥬~~
-
-	public void setNO2(int x) {
-		this.NO2 = x;
-	}
-
-	public void setOZ(int x) {
-		this.OZ = x;
-	}
-
-	public void setCO2(int x) {
-		this.CO2 = x;
-	}
-
-	public void setSO2(int x) {
-		this.SO2 = x;
-	}
-
-	public void setFd(int x) {
-		this.Fd = x;
-	}
-
-	public void setUd(int x) {
-		this.Ud = x;
-	}
-
-	@Override
-	public void paintComponent(Graphics g) { // drawXXX 말고 fillXXX는 내부가 채워짐.
-		super.paintComponent(g);
-
-		g.setColor(Color.BLACK);// 글자
-		g.drawString("NO2", 5, 20);// 내용,x좌표,y좌표
-		g.setColor(Color.ORANGE);
-		g.fillRect(80, 7, NO2, 20);
-
-		g.setColor(Color.BLACK);// 글자
-		g.drawString("OZ", 5, 50);// 내용,x좌표,y좌표
-		g.setColor(Color.GREEN);
-		g.fillRect(80, 37, OZ, 20);
-
-		g.setColor(Color.BLACK);// 글자
-		g.drawString("CO2", 5, 80);// 내용,x좌표,y좌표
-		g.setColor(Color.ORANGE);
-		g.fillRect(80, 67, CO2, 20);
-
-		g.setColor(Color.BLACK);// 글자
-		g.drawString("SO2", 5, 110);// 내용,x좌표,y좌표
-		g.setColor(Color.GREEN);
-		g.fillRect(80, 97, SO2, 20);
-
-		g.setColor(Color.BLACK);// 글자
-		g.drawString("Fd", 5, 140);// 내용,x좌표,y좌표
-		g.setColor(Color.ORANGE);
-		g.fillRect(80, 127, Fd, 20);
-
-		g.setColor(Color.BLACK);// 글자
-		g.drawString("Ud", 5, 170);// 내용,x좌표,y좌표
-		g.setColor(Color.GREEN);
-		g.fillRect(80, 157, Ud, 20);
-
-=======
-
-
-public class MainGraph {
-	public static void main(String args[]) {
-		JFrame frame = new JFrame("Pollution Graph");
-		frame.setLocation(500,200);
-		frame.setPreferredSize(new Dimension(400,350));
-		Container contentPane = frame.getContentPane();
-  
-		DrawingPanel drawingPanel = new DrawingPanel();
-		contentPane.add(drawingPanel, BorderLayout.CENTER);
-		//그래프를 그릴 패널
-  
-		JPanel controlPanel = new JPanel();
-		JTextField text1 = new JTextField(3);
-		JTextField text2 = new JTextField(3);
-		JTextField text3 = new JTextField(3);
-		JButton button = new JButton("그래프 그리기");
-		controlPanel.add(new JLabel("강서구"));
-		controlPanel.add(text1);
-		controlPanel.add(new JLabel("서대문구"));
-		controlPanel.add(text2);
-		controlPanel.add(new JLabel("강남구"));
-		controlPanel.add(text3);
-		controlPanel.add(button);
-		contentPane.add(controlPanel, BorderLayout.SOUTH);	
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		button.addActionListener(new DrawActionListener(text1,text2,text3,drawingPanel));
-		//"그래프 그리기" 버튼을 눌렀을때 작동 할 리스터등록
-		frame.pack();
-		frame.setVisible(true);
->>>>>>> branch 'master' of https://github.com/Kimyeongsan/Airpollution.git
-	}
-}
+import javax.swing.JTextField;
 
 //메인에 들어가는 꺾은선 그래프
 class MainGraph_Polygonal extends JPanel {
