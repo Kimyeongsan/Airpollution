470bc9cf44bf79adb9c2144a375565c0b319fa54
YeongSan <apua34@gmail.com>
2019-12-02 18:09:24 +0900

    add..pull

--- src/PollutionComparePanel.java
+++ src/PollutionComparePanel.java
@@ -37,7 +37,13 @@ public class PollutionComparePanel extends JPanel {
 		CompareNewLabel.setHorizontalAlignment(JLabel.CENTER);
 		CompareNewLabel.setBounds(192, 10, 245, 30);
 		this.add(CompareNewLabel);
-
+		/*
+		 * JPanel first_compareGraph = new JPanel();//// 그래프 1 들어갈 패널////////////
+		 * first_compareGraph.setBounds(38, 131, 228, 200);
+		 * first_compareGraph.setBorder(BorderFactory.createLineBorder(Color.red));
+		 * Comparepanel.add(first_compareGraph); // first_compareGraph.add(new
+		 * JLabel("NO2"));
+		 */
 		JLabel fisrt_compareLabel = new JLabel("지역선택A");
 		fisrt_compareLabel.setBounds(38, 64, 63, 15);
 		this.add(fisrt_compareLabel);
@@ -55,8 +61,12 @@ public class PollutionComparePanel extends JPanel {
 		JLabel lblNewLabel_2 = new JLabel("날짜 입력");
 		lblNewLabel_2.setBounds(38, 101, 63, 15);
 		this.add(lblNewLabel_2);
-
-	
+		/*
+		 * JPanel second_comparGraph = new JPanel();//// 그래프 2들어갈 패널///////////
+		 * second_comparGraph.setBounds(382, 131, 228, 200);
+		 * second_comparGraph.setBorder(BorderFactory.createLineBorder(Color.red));
+		 * Comparepanel.add(second_comparGraph);
+		 */
 		JLabel lblNewLabel_3 = new JLabel("지역선택B");
 		lblNewLabel_3.setBounds(360, 64, 68, 15);
 		this.add(lblNewLabel_3);
@@ -64,6 +74,15 @@ public class PollutionComparePanel extends JPanel {
 		JLabel lblNewLabel_4 = new JLabel("날짜 입력");
 		lblNewLabel_4.setBounds(360, 101, 68, 15);
 		this.add(lblNewLabel_4);
+		//
+
+//이식된 그래프 그려질 판넬
+
+//첫 비교 판넬에 추가해주기
+
+//      button.addActionListener(new DrawActionListener(text1, text2, text3, drawingPanel));
+
+		//
 
 		// 첫번째 날짜 입력
 		textField_D1 = new JTextField();
@@ -77,10 +96,6 @@ public class PollutionComparePanel extends JPanel {
 		this.add(textField_D2);
 		textField_D2.setColumns(10);
 
-<<<<<<< HEAD
-		// 적용버튼 생성 ^^7
-=======
->>>>>>> branch 'master' of https://github.com/Kimyeongsan/Airpollution.git
 		JButton btnCompareSet = new JButton("적용");
 
 		// 적용버튼 위치 및 판넬에 추가
@@ -111,7 +126,10 @@ public class PollutionComparePanel extends JPanel {
 	}
 
 	private void graphInit() {
-		Graphpanel = new GraphPollution_Compare();
+		// 그래프 시도해보자
+		// 비교 그래프 그려줄 클래스1
+		// 이거랑 같은 거 하나 더만들어서 2로 쓸꺼야
+		Graphpanel = new GraphPollution_Compare();//// 그래프 1 들어갈 패널////////////
 		Graphpanel.setBounds(38, 131, 228, 200);
 		Graphpanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		// 값 수정은 이런식으로
@@ -123,7 +141,7 @@ public class PollutionComparePanel extends JPanel {
 	}
 
 	private void graphInit_2() {
-		Graphpanel_2 = new GraphPollution_Compare();
+		Graphpanel_2 = new GraphPollution_Compare();//// 그래프 2들어갈 패널///////////
 		Graphpanel_2.setBounds(382, 131, 228, 200);
 		Graphpanel_2.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.add(Graphpanel_2);
